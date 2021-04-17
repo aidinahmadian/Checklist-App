65c1cab93c0531c9bc1e536f8bb631e2cfd848e4
Aidin <ahmadian_aidin@yahoo.com>
2021-04-17 16:19:46 +0430

    Checklist App in Swift 5

--- Checklist App/ViewController.swift
+++ Checklist App/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let items: [ChecklistItem] = [
+        "Get Milk",
+        "Get on a long run",
+        "Make Youtube Videos",
+        "Do Somthing Fun",
+        "Go on a long walk",
+        "Be Productive",
+    ].compactMap({
+        ChecklistItem(title: $0)
+    })
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Checklist"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.tableFooterView = UIView()
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    //Table Functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = item.title
+        cell.textLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        cell.accessoryType = item.isChecked ? .checkmark : .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = items[indexPath.row]
+        item.isChecked = !item.isChecked
+        tableView.reloadRows(at: [indexPath], with: .right)
+    }
 
 }
 
---
+++ Checklist App/checklistItem.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistItem.swift
+//  Checklist App
+//
+//  Created by Aidin Ahmadian on 4/17/21.
+//
+
+import UIKit
+
+class ChecklistItem {
+    let title: String
+    var isChecked: Bool = false
+    
+    init(title: String) {
+        self.title = title
+    }
+}
